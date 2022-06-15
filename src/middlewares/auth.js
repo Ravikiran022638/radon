5848cb7b0155cf0c38412ed6a3a5ff9ad027564b
Ravikiran022638 <kiranravi022@gmail.com>
2022-06-15 14:25:43 +0530

    auth3

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -20,4 +20,17 @@ const authMiddleware = async function(req , res , next){
     next()
     }
 
+    const authenticate = function(req, req, next) {
+      //check the token in request header
+      //validate this token
+  
+      next()
+  }
+  
+  
+  const authorise = function(req, res, next) {
+      // comapre the logged in user's id and the id in request
+      next()
+  }
+
 module.exports.authMiddleware= authMiddleware
